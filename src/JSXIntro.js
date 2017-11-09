756f7fa5539144dbd563269defab100a7e8c72d5
Brian Donnoe <dunow33@hotmail.com>
2017-11-08 23:11:37 -0500

    first change

---
+++ src/JSXIntro.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class JSXIntro extends Component {
+	render() {
+		return (
+			<div>
+				<h3>JSX</h3>
+				<ul>
+					<li>hello</li>
+					<li>this is react....isnt it neat???</li>
+				</ul>
+				<hr />
+			</div>
+		);
+	}
+}
+
+export default JSXIntro;
